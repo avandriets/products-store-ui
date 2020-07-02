d748b314ab17ca2265e643dd1a0a92b1213ed499
Alexander Andriets <Alexander.Andriets@acquia.com>
2020-07-02 22:02:38 +0300

    fix tslint rules

--- apps/products-ui/src/app/app.module.ts
+++ apps/products-ui/src/app/app.module.ts
@@ -10,4 +10,5 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
