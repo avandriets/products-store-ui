4807cf8df61232fe78e762a08e7932e3f0905ef4
Alexander Andriets <Alexander.Andriets@acquia.com>
2020-11-06 13:28:39 +0200

    add pagination

--- libs/products-catalog-store/src/lib/services/products.service.ts
+++ libs/products-catalog-store/src/lib/services/products.service.ts
@@ -1,16 +1,13 @@
 import { Injectable } from '@angular/core';
-import {
-  EntityCollectionServiceBase,
-  EntityCollectionServiceElementsFactory,
-} from '@ngrx/data';
-import { Status } from '@products-store-ui/products-core';
-import { Observable, combineLatest } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { EntityCollectionServiceElementsFactory } from '@ngrx/data';
+import { createSelector, select } from '@ngrx/store';
+import { BaseEntityCollectionService } from '@products-store-ui/products-core';
+import { Observable } from 'rxjs';
 
 import { Product } from '../interfaces';
 
 @Injectable({ providedIn: 'root' })
-export class ProductService extends EntityCollectionServiceBase<Product> {
+export class ProductService extends BaseEntityCollectionService<Product> {
 
   public constructor(
     serviceElementsFactory: EntityCollectionServiceElementsFactory,
@@ -18,65 +15,16 @@ export class ProductService extends EntityCollectionServiceBase<Product> {
     super('Product', serviceElementsFactory);
   }
 
-  public get status$(): Observable<Status> {
-    return combineLatest([
-      this.loaded$,
-      this.loading$,
-      this.errors$.pipe(
-        map(errors => errors.payload.data?.error?.error),
-        startWith(null),
-      ),
-    ]).pipe(
-      map(([resolved, pending, err]) => ({
-        resolved,
-        pending,
-        rejected: !!err,
-        err,
-      })),
+  public getTotalCount$(): Observable<number> {
+
+    const paginationSelector =
+      createSelector(
+        this.selectors.selectEntityCache, state => (state.ContentSummary as any)?.pagination?.count ?? 0,
+      );
+
+    return this.store.pipe(
+      select(paginationSelector),
     );
   }
-
-  // /**
-  //  * Get server data changed
-  //  */
-  // public get serverDataChanged$(): Observable<any> {
-  //   return this.entityActions$.pipe(
-  //     ofEntityOp([
-  //       EntityOp.SAVE_ADD_ONE_SUCCESS,
-  //       EntityOp.SAVE_ADD_MANY_SUCCESS,
-  //       EntityOp.SAVE_UPDATE_ONE_SUCCESS,
-  //       EntityOp.SAVE_UPDATE_MANY_SUCCESS,
-  //       EntityOp.SAVE_DELETE_ONE_SUCCESS,
-  //       EntityOp.SAVE_DELETE_MANY_SUCCESS,
-  //     ]),
-  //     startWith(null),
-  //   );
-  // }
-
-  // public createNew(): Observable<Product> {
-  //
-  //   const result: Product = {
-  //     id: '',
-  //     title: '',
-  //     description: '',
-  //   };
-  //
-  //   this.setLoaded(true);
-  //
-  //   return of(result);
-  //
-  // }
-
-  // public getWithQuery(queryParams: QueryParams): Observable<Product[]> {
-  //
-  //   this.clearCache();
-  //
-  //   return super.getWithQuery(queryParams);
-  //
-  // }
-  //
-  // public createOrUpdate(site: Product): void {
-  //   this.createAndDispatch(EntityOp.SAVE_ADD_MANY, [site]);
-  // }
 
 }
